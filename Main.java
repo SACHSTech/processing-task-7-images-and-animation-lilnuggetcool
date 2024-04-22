14969493132ea9ab20374a3853aa128398970990
lilnuggetcool <Ara.Torossian25@ycdsbk12.ca>
2024-04-22 14:17:52 -0400

    blank

--- Main.java
+++ Main.java
@@ -3,14 +3,12 @@ import processing.core.PApplet;
 /**
  * Main class to execute sketch
  * @author 
- *
  */
 class Main {
   public static void main(String[] args) {
     
     String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch();
-	  PApplet.runSketch(processingArgs, mySketch);
+    Sketch mySketch = new Sketch();
+    PApplet.runSketch(processingArgs, mySketch);
   }
-  
 }
